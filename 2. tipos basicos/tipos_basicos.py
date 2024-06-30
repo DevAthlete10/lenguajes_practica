402f1b1bb51e6d4897560edb036c57148cb610c8
Cristóbal Sandoval <devAthete10@gmail.com>
2024-06-30 17:39:55 -0400

    UPDATE tipos_basicos

---
+++ 2. tipos basicos/tipos_basicos.py
@@ -0,0 +1,15 @@
+""" Modulos """
+NUMERO_ENTERO = 2
+NUMERO_DECIMAL = 2.2
+CADENA_DE_CARACTERES = "cristobal"
+VERDADERO = True
+FALSO = False
+
+
+print(
+    f" Entero => {NUMERO_ENTERO}",
+    f" Decimal => {NUMERO_DECIMAL}",
+    f" Cadena de caracteres => {CADENA_DE_CARACTERES}",
+    f" Verdadero => {VERDADERO}",
+    f" Falso => {FALSO}",
+    )
