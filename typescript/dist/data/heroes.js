b842059e216884d072a98bbf0ee40bacfde09263
Dev athlete <devathlete10@gmail.com>
2024-10-05 17:47:34 -0300

    update folders

---
+++ typescript/dist/data/heroes.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.heroes = void 0;
+exports.heroes = [{
+        id: 1,
+        name: "Spiderman",
+        owner: "Marvel"
+    },
+    {
+        id: 2,
+        name: "Batman",
+        owner: "DC"
+    },
+    {
+        id: 3,
+        name: "Superman",
+        owner: "DC"
+    },
+];
